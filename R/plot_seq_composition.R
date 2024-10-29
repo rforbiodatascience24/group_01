9b7758ed5c2b1da3baa743e44dd632344c03d701
LuisaWeisch <luisa.weisch@outlook.com>
2024-10-29 11:22:30 +0100

    Implementation of function to plot the sequence composition

--- R/plot_seq_composition.R
+++ R/plot_seq_composition.R
@@ -7,17 +7,20 @@
 #'
 #' @examples plot_seq_composition("AGTGTAA")
 plot_seq_composition <- function(sequence){
+  # Taking all the unique letters in a sequence
   chars <- sequence |>
     stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
     as.character() |>
     unique()
 
+  # Counting how often these unique characters appear in the sequence
   counts <- sapply(chars, function(amino_acid) stringr::str_count(string = sequence, pattern =  amino_acid)) |>
     as.data.frame()
 
   colnames(counts) <- c("Counts")
   counts[["Sequence"]] <- rownames(counts)
 
+  # Visualizing the counts in a plot
   composition_plot <- counts |>
     ggplot2::ggplot(ggplot2::aes(x = Sequence, y = Counts, fill = Sequence)) +
     ggplot2::geom_col() +
