887d93b71efd32fafd169b8bc128d0362e3cc1e0
dalofa <dalofa@dtu.dk>
2024-10-29 09:15:07 +0100

    Add codon_to_aa and related documentation

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title
+#'
+#' A named vector containing codons and the amino acids they are translated to
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_to_aa"
